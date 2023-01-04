c23b5e9557c9d1a4bae914f85b2abc33254aa66a
Isaac Way <iway1@vols.utk.edu>
2023-01-04 07:15:13 -0600

    Update logging.ts

--- src/logging.ts
+++ src/logging.ts
@@ -16,7 +16,7 @@ export function printWarningsForSchema(type: RTFSupportedZodTypes) {
     type._def.typeName === ZodFirstPartyTypeKind.ZodEnum
   ) {
     err(
-      "support for z.enum() is deprecated and will be removed in future versions. Prefer z.string() / z.number() etc for dropdowns, selects, and radio buttons. \nSee https://github.com/iway1/react-ts-form/field-examples.md for examples"
+      "support for z.enum() is deprecated and will be removed in future versions. Prefer z.string() / z.number() etc for dropdowns, selects, and radio buttons. \nSee https://github.com/iway1/react-ts-form/blob/main/field-examples.md for examples"
     );
     shownWarnings.enum = true;
   }
@@ -25,7 +25,7 @@ export function printWarningsForSchema(type: RTFSupportedZodTypes) {
 export function printUseEnumWarning() {
   if (!shownWarnings.useEnum) {
     err(
-      "useEnumValues is deprecated and will be removed in future versions. See https://github.com/iway1/react-ts-form/field-examples.md for examples of how to implement selects/dropdowns etc without enums."
+      "useEnumValues is deprecated and will be removed in future versions. See https://github.com/iway1/react-ts-form/blob/main/field-examples.md for examples of how to implement selects/dropdowns etc without enums."
     );
     shownWarnings.useEnum = true;
   }
