9aea67539a79bc16367f71f40d5752bc27d9b970
Isaac Way <iway1@vols.utk.edu>
2023-03-10 19:23:01 -0600

    docusaurus config

--- www/docusaurus.config.js
+++ www/docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://react-ts-form.vercel.app",
+  url: "https://react-ts-form.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -42,7 +42,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/iway1/react-ts-form/tree/main/packages/create-docusaurus/templates/shared/",
         },
 
         theme: {
