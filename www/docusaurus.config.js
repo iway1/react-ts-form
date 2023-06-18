fe5d665e1633884b43d3ded753302a28ba073d3a
Isaac Way <iway1@vols.utk.edu>
2023-06-18 11:47:47 -0400

    fixes docs edit page link

--- www/docusaurus.config.js
+++ www/docusaurus.config.js
@@ -41,7 +41,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/iway1/react-ts-form/",
+          editUrl: "https://github.com/iway1/react-ts-form/tree/main/www/",
         },
 
         theme: {
