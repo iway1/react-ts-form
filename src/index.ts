9f8f194467a03cc0b5e09c331bd87e68347b2cfd
Sterling Camden <rusty.camden@gmail.com>
2023-08-01 09:27:49 -1000

    export the new function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { createUniqueFieldSchema } from "./createFieldSchema";
-export { createTsForm } from "./createSchemaForm";
+export { createTsForm, createTsFormAndFragment } from "./createSchemaForm";
 export {
   useDescription,
   useReqDescription,
